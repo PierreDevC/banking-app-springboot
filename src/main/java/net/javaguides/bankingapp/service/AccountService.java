73e2fb982433c7bbbd8d94ec0011822ddd4373ff
P-S.Cypre <pscypre@gmail.com>
2025-01-18 16:28:27 -0500

    init commit

---
+++ src/main/java/net/javaguides/bankingapp/service/AccountService.java
@@ -0,0 +1,8 @@
+package net.javaguides.bankingapp.service;
+
+import net.javaguides.bankingapp.dto.AccountDto;
+
+public interface AccountService {
+
+    AccountDto createAccount(AccountDto accountDto);
+}
